9d5e97b099608638cffbac9a027cc326a12166a4
Eric Fischer <efischer78@yahoo.fr>
2023-08-12 10:39:19 +0200

    ok

--- templates/catalog.sh
+++ templates/catalog.sh
@@ -1,37 +1,37 @@
 #!/bin/bash
 set -e
 
-if [ ! -d {{ .pack.xbeeOut }}/etc/xml ]; then install -v -m755 -d {{ .pack.xbeeOut }}/etc/xml; fi &&
-if [ ! -f {{ .pack.xbeeOut }}/etc/xml/catalog ]; then
-    xmlcatalog --noout --create {{ .pack.xbeeOut }}/etc/xml/catalog
+if [ ! -d {{ .xbee.out }}/etc/xml ]; then install -v -m755 -d {{ .xbee.out }}/etc/xml; fi &&
+if [ ! -f {{ .xbee.out }}/etc/xml/catalog ]; then
+    xmlcatalog --noout --create {{ .xbee.out }}/etc/xml/catalog
 fi &&
 
 xmlcatalog --noout --add "rewriteSystem" \
            "https://cdn.docbook.org/release/xsl-nons/1.79.2" \
            "/usr/share/xml/docbook/xsl-stylesheets-nons-1.79.2" \
-    {{ .pack.xbeeOut }}/etc/xml/catalog &&
+    {{ .xbee.out }}/etc/xml/catalog &&
 
 xmlcatalog --noout --add "rewriteURI" \
            "https://cdn.docbook.org/release/xsl-nons/1.79.2" \
            "/usr/share/xml/docbook/xsl-stylesheets-nons-1.79.2" \
-    {{ .pack.xbeeOut }}/etc/xml/catalog &&
+    {{ .xbee.out }}/etc/xml/catalog &&
 
 xmlcatalog --noout --add "rewriteSystem" \
            "https://cdn.docbook.org/release/xsl-nons/current" \
            "/usr/share/xml/docbook/xsl-stylesheets-nons-1.79.2" \
-    {{ .pack.xbeeOut }}/etc/xml/catalog &&
+    {{ .xbee.out }}/etc/xml/catalog &&
 
 xmlcatalog --noout --add "rewriteURI" \
            "https://cdn.docbook.org/release/xsl-nons/current" \
            "/usr/share/xml/docbook/xsl-stylesheets-nons-1.79.2" \
-    {{ .pack.xbeeOut }}/etc/xml/catalog &&
+    {{ .xbee.out }}/etc/xml/catalog &&
 
 xmlcatalog --noout --add "rewriteSystem" \
            "http://docbook.sourceforge.net/release/xsl/current" \
            "/usr/share/xml/docbook/xsl-stylesheets-nons-1.79.2" \
-    {{ .pack.xbeeOut }}/etc/xml/catalog &&
+    {{ .xbee.out }}/etc/xml/catalog &&
 
 xmlcatalog --noout --add "rewriteURI" \
            "http://docbook.sourceforge.net/release/xsl/current" \
            "/usr/share/xml/docbook/xsl-stylesheets-nons-1.79.2" \
-    {{ .pack.xbeeOut }}/etc/xml/catalog
+    {{ .xbee.out }}/etc/xml/catalog
